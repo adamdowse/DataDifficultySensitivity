fixed test scaling issue

DataHandler.py
@@ -20,9 +20,10 @@ class DataHandler(tf.keras.utils.Sequence):
 
         #download the test dataset and prepare it
         self.test_tfds,self.test_info = self.download_dataset(train=False)
-        self.test_tfds = self.test_tfds.batch(self.config.batch_size)
         self.test_tfds = self.test_tfds.map(lambda img,label: (tf.cast(img,tf.float32),tf.one_hot(label,self.num_classes)))
-        
+        self.test_tfds = self.test_tfds.map(lambda img,label: (tf.cast((img-self.min_val)/(self.max_val-self.min_val),tf.float32),label))
+        self.test_tfds = self.test_tfds.batch(self.config.batch_size)
+
         self.total_train_data_points = self.train_info.splits['train'].num_examples
         self.current_train_data_points = 0
         self.current_train_batch_num = 0
@@ -76,14 +77,15 @@ class DataHandler(tf.keras.utils.Sequence):
 
         #find max and min of img and normalize
         t = time.time()
-        max_val = np.max(DS_imgs)
-        min_val = np.min(DS_imgs)
-        DS_imgs = (DS_imgs-min_val)/(max_val-min_val)
+        self.max_val = np.max(DS_imgs)
+        self.min_val = np.min(DS_imgs)
+        DS_imgs = (DS_imgs-self.min_val)/(self.max_val-self.min_val)
         print('--> Numpy Time: ',time.time()-t)
 
         #prepare tf dataset
         t = time.time()
-        tf_ds = tf_ds.map(lambda img,label: (tf.cast((img-min_val)/(max_val-min_val),tf.float32),tf.one_hot(label,self.num_classes)))
+        tf_ds = tf_ds.map(lambda img,label: (tf.cast((img-self.min_val)/(self.max_val-self.min_val),tf.float32),tf.one_hot(label,self.num_classes)))
+        print('--> DS min: ',self.min_val,' max: ',self.max_val)
         tf_ds = tf_ds.batch(bs)
         print('--> TFDS Time: ',time.time()-t)
         return DS_imgs,DS_labels,DS_loss,tf_ds

Models.py
@@ -166,36 +166,37 @@ class Models():
 
         elif self.config.model_name == "TFCNN":
             self.model = tf.keras.Sequential([
-                tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=self.dataset_info.features['image'].shape, kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=self.dataset_info.features['image'].shape),
                 tf.keras.layers.MaxPool2D((2,2)),
-                tf.keras.layers.Conv2D(64,(3,3),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
                 tf.keras.layers.MaxPool2D((2,2)),
-                tf.keras.layers.Conv2D(64,(3,3),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
                 tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(64,activation='relu', kernel_initializer=initialiser),
-                tf.keras.layers.Dense(self.dataset_info.features['label'].num_classes,activation='softmax', kernel_initializer=initialiser)
+                tf.keras.layers.Dense(64,activation='relu'),
+                tf.keras.layers.Dense(self.dataset_info.features['label'].num_classes,activation='softmax')
             ])
             self.output_is_logits = False
         elif self.config.model_name == "ACLCNN":
             self.model = tf.keras.Sequential([
-                tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=self.dataset_info.features['image'].shape, kernel_initializer=initialiser),
-                tf.keras.layers.Conv2D(32,(3,3),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(32,(3,3),activation='elu',input_shape=self.dataset_info.features['image'].shape, kernel_initializer=initialiser,padding='same'),
+                tf.keras.layers.Conv2D(32,(3,3),activation='elu', kernel_initializer=initialiser,padding='same'),
                 tf.keras.layers.MaxPool2D((2,2)),
                 tf.keras.layers.Dropout(0.25),
-                tf.keras.layers.Conv2D(64,(3,3),activation='relu', kernel_initializer=initialiser),
-                tf.keras.layers.Conv2D(64,(3,3),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(64,(3,3),activation='elu', kernel_initializer=initialiser,padding='same'),
+                tf.keras.layers.Conv2D(64,(3,3),activation='elu', kernel_initializer=initialiser,padding='same'),
                 tf.keras.layers.MaxPool2D((2,2)),
                 tf.keras.layers.Dropout(0.25),
-                tf.kears.layers.Conv2D(128,(3,3),activation='relu', kernel_initializer=initialiser),
-                tf.kears.layers.Conv2D(128,(3,3),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(128,(3,3),activation='elu', kernel_initializer=initialiser,padding='same'),
+                tf.keras.layers.Conv2D(128,(3,3),activation='elu', kernel_initializer=initialiser,padding='same'),
                 tf.keras.layers.MaxPool2D((2,2)),
                 tf.keras.layers.Dropout(0.25),
-                tf.keras.layers.Conv2D(256,(2,2),activation='relu', kernel_initializer=initialiser),
-                tf.keras.layers.Conv2D(256,(2,2),activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Conv2D(256,(2,2),activation='elu', kernel_initializer=initialiser,padding='same'),
+                tf.keras.layers.Conv2D(256,(2,2),activation='elu', kernel_initializer=initialiser,padding='same'),
                 tf.keras.layers.MaxPool2D((2,2)),
                 tf.keras.layers.Dropout(0.25),
                 tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(20,activation='relu', kernel_initializer=initialiser),
+                tf.keras.layers.Dense(512,activation='elu', kernel_initializer=initialiser),
+                tf.keras.layers.Dropout(0.5),
                 tf.keras.layers.Dense(self.dataset_info.features['label'].num_classes,activation='softmax', kernel_initializer=initialiser)])
             self.output_is_logits = False
         else:
@@ -254,7 +255,7 @@ class Models():
             return False
 
     def log_metrics(self):
-        wandb.log({'train_loss':self.train_loss_metric.result(),'train_acc':self.train_acc_metric.result(),'train_prec':self.train_prec_metric.result(),'train_rec':self.train_rec_metric.result(),'test_loss':self.test_results[0],'test_acc':self.test_results[1],'test_prec':self.test_results[2],'test_rec':self.test_results[3],'lr':self.lr},step=self.epoch_num)
+        wandb.log({'train_loss':self.train_loss_metric.result(),'train_acc':self.train_acc_metric.result(),'train_prec':self.train_prec_metric.result(),'train_rec':self.train_rec_metric.result(),'test_loss':self.test_results[0],'test_acc':self.test_results[1],'test_prec':self.test_results[2],'test_rec':self.test_results[3],'lr':self.optimizer.learning_rate.numpy()},step=self.epoch_num)
 
     def calc_FIM(self,dataset):
         #this needs to define the FIM