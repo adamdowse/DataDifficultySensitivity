720242857c77b6d6ed485bb102905d57155fb384
adamdowse <adamdowse@hotmail.co.uk>
2023-09-16 21:50:53 +0000

    CIFAR10 downloader

---
+++ Dataset_creator.py
@@ -0,0 +1,276 @@
+
+import os
+import pandas as pd
+import numpy as np
+import shutil
+import cv2
+from sklearn import model_selection
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import PIL
+
+
+#build and save the img dataset in the correct format
+def build_HAM(root_dir,preaugment_size=1000):
+    root_dir = os.path.join(data_dir,"HAM10000")
+    meta_data_dir = os.path.join(root_dir,"HAM10000_metadata.csv")
+    data_dir = os.path.join(root_dir,"data")
+
+    train_size = 0.85 #TODO CHECK THIS
+    print("Train size: ",train_size)
+
+
+    #check if the augmented data exists
+    if os.path.exists(os.path.join(root_dir,"C_"+str(preaugment_size))):
+        raise ValueError("The augmented data does exist, this does not need to be run")
+    #check if the original data exists
+    if not os.path.exists(meta_data_dir):
+        raise ValueError("The original metadata csv does not exist, please check the path")
+    #check if the original data exists
+    if not os.path.exists(data_dir):
+        raise ValueError("The original data file does not exist, please check the path")
+    
+    #adapted from SoftAttention paper
+    df = pd.read_csv(meta_data_dir)
+    df_count = df.groupby('lesion_id').count()
+    df_count = df_count[df_count['dx'] == 1]
+    df_count.reset_index(inplace=True)
+
+    def duplicates(x):
+        unique = set(df_count['lesion_id'])
+        if x in unique:
+            return 'no' 
+        else:
+            return 'duplicates'
+    
+    df['is_duplicate'] = df['lesion_id'].apply(duplicates)
+    df_count = df[df['is_duplicate'] == 'no']
+    train, test_df = model_selection.train_test_split(df_count, test_size=1-train_size, stratify=df_count['dx'])
+
+    def identify_trainOrtest(x):
+        test_data = set(test_df['image_id'])
+        if str(x) in test_data:
+            return 'test'
+        else:
+            return 'train'
+
+    #creating df for train and test
+    df['train_test_split'] = df['image_id'].apply(identify_trainOrtest)
+    train_df = df[df['train_test_split'] == 'train'].copy()
+    test_df = df[df['train_test_split'] == 'test'].copy()
+
+    # Image id of train and test images
+    train_list = list(train_df['image_id'])
+    test_list = list(test_df['image_id'])
+
+    print('Train DF sise: ', len(train_list))
+    print(train_df.head())
+    print('Test DF size: ', len(test_list))
+    print(test_df.head())
+
+    #calculate number of images in each class
+    classes = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
+    num_classes = len(classes)
+    print('Number of classes: ', num_classes)
+    train_class_count = {'akiec':0, 'bcc':0, 'bkl':0, 'df':0, 'mel':0, 'nv':0, 'vasc':0}
+    train_df.set_index('image_id', inplace=True)
+    for img in train_list:
+        label = train_df.loc[img, 'dx']
+        train_class_count[label] += 1
+    print('Original Train Class count: ', train_class_count)
+
+    test_class_count = {'akiec':0, 'bcc':0, 'bkl':0, 'df':0, 'mel':0, 'nv':0, 'vasc':0}
+    test_df.set_index('image_id', inplace=True)
+    for img in test_list:
+        label = test_df.loc[img, 'dx']
+        test_class_count[label] += 1
+    
+    print('Original Test Class count: ', test_class_count)
+
+    # Set the image_id as the index in data_pd
+    df.set_index('image_id', inplace=True)
+
+    #if preaugment is true then augment and save images to directory and update df with new files
+    if preaugment_size == 0:
+        print('No Preaugmentation')
+        #copy all training images to a new folder
+        if os.path.exists(os.path.join(root_dir,'C_'+str(preaugment_size))):
+            shutil.rmtree(os.path.join(root_dir,'C_'+str(preaugment_size)))
+        os.mkdir(os.path.join(root_dir,'C_'+str(preaugment_size)))
+        for img in train_list:
+            shutil.copy(os.path.join(root_dir,'data',img+'.jpg'),os.path.join(root_dir,'C_'+str(preaugment_size),img+'.jpg'))
+
+    else:
+        print('Preaugmenting images')
+        
+        #count remaining images to add
+        remaining_class_count = train_class_count.copy()
+        for i in classes:
+            remaining_class_count[i] = preaugment_size - train_class_count[i]
+        print('Remaining Class Count To Add: ', remaining_class_count)
+
+        #make temp folder for aug images
+        if os.path.exists(os.path.join(root_dir,'aug_dir')):
+            shutil.rmtree(os.path.join(root_dir,'aug_dir'))
+        os.mkdir(os.path.join(root_dir,'aug_dir'))
+        for i in classes:
+            os.mkdir(os.path.join(root_dir,'aug_dir',i))
+
+        #create final data folder
+        if os.path.exists(os.path.join(root_dir,'C_'+str(preaugment_size))):
+            shutil.rmtree(os.path.join(root_dir,'C_'+str(preaugment_size)))
+        os.mkdir(os.path.join(root_dir,'C_'+str(preaugment_size)))
+
+        #Augment imgs
+        for class_name in classes:
+            print('Aug Class: ',class_name)
+            #make temp folder for original images
+            if os.path.exists(os.path.join(root_dir,'temp_dir')):
+                shutil.rmtree(os.path.join(root_dir,'temp_dir'))
+            os.mkdir(os.path.join(root_dir,'temp_dir'))
+            os.mkdir(os.path.join(root_dir,'temp_dir',class_name))
+
+            #add original imgs to temp folders with class structure
+            class_df = train_df.loc[train_df['dx']==class_name].copy()
+            class_list = class_df.index
+            for img in class_list:
+                file_name = img+'.jpg'
+                img_path = os.path.join(root_dir,'data',file_name)
+                target_path = os.path.join(root_dir,'temp_dir',class_name,file_name)
+                shutil.copy(img_path,target_path)
+
+            #create data generator
+            data_aug_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=180,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                zoom_range=0.1,
+                horizontal_flip=True,
+                vertical_flip=True,
+                fill_mode='nearest'
+            )
+            #create class seperated data generators
+            source_path = os.path.join(root_dir,'temp_dir')
+            save_path = os.path.join(root_dir,'aug_dir',class_name)
+
+            #add agumented images to folder to make up a specified number
+            num_new_items = remaining_class_count[class_name]
+            source_list = os.listdir(os.path.join(source_path,class_name))
+            for i in range(0,num_new_items):
+                #choose random file to augment
+                f_name = source_list[np.random.randint(0,len(source_list))]
+                img = cv2.imread(os.path.join(source_path,class_name,f_name))
+                aug_img_it = data_aug_gen.flow(x=np.expand_dims(img,0),batch_size=1)
+                aug_img = next(aug_img_it)
+                cv2.imwrite(os.path.join(save_path,'aug'+str(np.random.randint(100000))+'_'+f_name),np.squeeze(aug_img,axis=0))
+
+            print('-->AugImgs: ',len(os.listdir(save_path)))
+            print('--> OGImgs: ',len(os.listdir(os.path.join(source_path,class_name))))
+
+
+            #transfer all of aug class to thefinal data folder
+            for img in os.listdir(save_path):
+                shutil.move(os.path.join(save_path,img),os.path.join(root_dir,'C_'+str(preaugment_size),img))
+
+            #transfer all of og class to thefinal data folder
+            s_path = os.path.join(source_path,class_name)
+            for img in os.listdir(s_path):
+                shutil.move(os.path.join(s_path,img),os.path.join(root_dir,'C_'+str(preaugment_size),img))
+
+        #delete temp_dir
+        shutil.rmtree(os.path.join(root_dir,'temp_dir'))
+
+        #delete aug_dir
+        shutil.rmtree(os.path.join(root_dir,'aug_dir'))
+
+        #We now have a folder called 'C_1000' with 1000 images per class all in one folder
+        #update the train_df with the new images
+        #get list of images
+        train_list = os.listdir(os.path.join(root_dir,'C_'+str(preaugment_size)))
+        #if the image is not in the train_df then add it to the train_df
+        for img in train_list:
+            img = img[:-4]
+            if img not in train_df.index:
+                #copy row from index - aug
+                m_img = img.split('_')
+                m_img = m_img[1] + '_'+ m_img[2]
+                new_img_row = train_df.loc[m_img].to_numpy(copy=True)
+                #add new row to train_df with new index
+                train_df.loc[img] = new_img_row
+        
+    #update train_class_count
+    train_class_count = {'akiec':0, 'bcc':0, 'bkl':0, 'df':0, 'mel':0, 'nv':0, 'vasc':0}
+    
+    train_list = list(train_df.index)
+    train_class_count = {'akiec':0, 'bcc':0, 'bkl':0, 'df':0, 'mel':0, 'nv':0, 'vasc':0}
+    for img in train_list:
+        label = train_df.loc[img, 'dx']
+        train_class_count[label] += 1
+    print('Updated Train Class count via dir: ', train_class_count)
+
+    print('Total Train Imgs: ',len(train_list))
+    data_dir = os.path.join(root_dir,'C_'+str(preaugment_size))
+
+    #change df to [id, img_name, label] format
+    train_df.reset_index(inplace=True)
+    train_df = train_df[['image_id','dx']]
+    train_df.columns = ['image_id','label']
+    test_df.reset_index(inplace=True)
+    test_df = test_df[['image_id','dx']]
+    test_df.columns = ['image_id','label']
+    train_df.to_csv(os.path.join(root_dir,'C_'+str(preaugment_size)+'trainmetadata.csv'))
+    test_df.to_csv(os.path.join(root_dir,'C_'+str(preaugment_size)+'testmetadata.csv'))
+
+def build_CIFAR10(root_dir,preaugment_size=0):
+    #save the cifar10 tfds dataset to the folder and create the metadata csv files
+    root_dir = os.path.join(root_dir,"CIFAR10")
+    data_dir = os.path.join(root_dir,"C_"+str(preaugment_size))
+    test_dir = os.path.join(root_dir,"data")
+    
+    #check if the augmented data exists
+    if os.path.exists(data_dir):
+        raise ValueError("The augmented data does exist, this does not need to be run")
+    
+    #build the data dir
+    os.mkdir(data_dir)
+
+    #create the metadata csv file
+    train_df = pd.DataFrame(columns=['image_id','label'])
+
+    #download the train dataset
+    cifar10_ds, info = tfds.load('cifar10',split=['train'],data_dir=root_dir,with_info=True,download=True,as_supervised=True,shuffle_files=False)
+    iterator = iter(cifar10_ds)
+    class_names = info.features['label'].names
+    c = 0
+    for x,y in next(iterator):
+        name = str(class_names[y.numpy()])+'_'+str(c)
+        PIL.Image.fromarray(x.numpy()).save(os.path.join(data_dir,name +'.jpg'))
+        train_df.loc[c] = [name,class_names[y.numpy()]]
+        c += 1
+    print(train_df.head())
+
+    #create the test metadata csv file
+    test_df = pd.DataFrame(columns=['image_id','label'])
+
+    #create the test file dir
+    os.mkdir(test_dir)
+
+    #download the test dataset
+    cifar10_ds, info = tfds.load('cifar10',split=['test'],data_dir=root_dir,with_info=True,download=True,as_supervised=True,shuffle_files=False)
+    iterator = iter(cifar10_ds)
+    class_names = info.features['label'].names
+    c = 0
+    for x,y in next(iterator):
+        name = str(class_names[y.numpy()])+'_'+str(c)
+        PIL.Image.fromarray(x.numpy()).save(os.path.join(test_dir,name +'.jpg'))
+        test_df.loc[c] = [name,class_names[y.numpy()]]
+        c += 1
+    print(test_df.head())
+
+    #save the metadata csv files
+    train_df.to_csv(os.path.join(root_dir,"trainmetadata.csv"))
+    test_df.to_csv(os.path.join(root_dir,"testmetadata.csv"))
+
+
+if __name__ == "__main__":
+    build_CIFAR10('/com.docker.devenvironments.code/',preaugment_size=0)
--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 #testing the generator on multi gpus
 import os
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import pandas as pd
 import numpy as np
+import time
 
 
 
 
 def create_data():
     #Need to add function or other file to build the datasets
+    #add option method to cull the data to the number of images wanted
     return
 
-class HAM10000Data():
-    def __init__(self,strategy,data_dir,preaugment_size):
+class Data():
+    def __init__(self,strategy,data_dir,preaugment_size,img_size):
         #requires the file stucture to be
         #data_dir
-        #   -HAM10000_metadata.csv (original metadata)
         #   -data
         #       -all images
         #
@@ -23,10 +25,13 @@ class HAM10000Data():
         #       -all aug images
         #   -C_1000testmetadata.csv (test metadata)
         #   -C_1000trainmetadata.csv (train metadata)
+        #
+        #   -C_0 (example of dataset with 0 augmented images per class)(this is the original data)
 
         self.strategy = strategy
         self.data_dir = data_dir
         self.preaugment_size = preaugment_size
+        self.img_size = img_size #[244,244,3]HAM [32,32,3]CIFAR10
 
         #check if the data is in the correct format and the augmented data wanted exists
         if not self.__check_dirs():
@@ -43,6 +48,8 @@ class HAM10000Data():
         #calculate the number of classes
         self.num_classes = len(self.trainmetadata['label'].unique())
         print("Number of classes: ",self.num_classes)
+        self.class_names = self.trainmetadata['label'].unique().tolist()
+        print("Class names: ",self.class_names)
 
         #count the number of images in the train and test data per class
         self.train_class_count = self.trainmetadata['label'].value_counts()
@@ -51,18 +58,19 @@ class HAM10000Data():
         print("Test class count: ",self.test_class_count)
 
         #create a master list of all image names and labels
-        self.train_img_names = self.trainmetadata['image_id'].tolist()
-        self.train_img_labels = self.trainmetadata['label'].tolist()
-        self.train_img_labels = tf.keras.utils.to_categorical(self.train_img_labels, num_classes=self.num_classes, dtype='float32') #one hot encode the labels (might need to be int32)
+        self.train_img_names = np.array(self.trainmetadata['image_id'].tolist())
+        self.train_img_labels = np.array(self.trainmetadata['label'].tolist())
+        #convert to int labels
+        self.train_img_labels = np.array([self.class_names.index(i) for i in self.train_img_labels])
         
-        self.test_img_names = self.testmetadata['image_id'].tolist()
-        self.test_img_labels = self.testmetadata['label'].tolist()
-        self.test_img_labels = tf.keras.utils.to_categorical(self.test_img_labels, num_classes=self.num_classes, dtype='float32') #one hot encode the labels (might need to be int32)
+        self.test_img_names = np.array(self.testmetadata['image_id'].tolist())
+        self.test_img_labels = np.array(self.testmetadata['label'].tolist())
+        #convert to int labels
+        self.test_img_labels = np.array([self.class_names.index(i) for i in self.test_img_labels]) 
 
         #create index mask for train and test data
         self.train_index_mask = np.array([True]*len(self.train_img_names))
         self.test_index_mask = np.array([True]*len(self.test_img_names)) #this should always be true for all the data
-
 
     def __check_dirs(self):
         #returns true if the data is in the correct format
@@ -84,8 +92,19 @@ class HAM10000Data():
         else:
             return True
         
-    
-    def reduce_data(self,method):
+    def get_loss(self,model,bs=12):
+        #get the loss of the data
+        #build dataset
+        dataset, num_batches = self.init_data(bs,train=True,distributed=False,shuffle=False)
+        #get the loss
+        iterator = iter(dataset)
+        losses = np.array([])
+        for i in range(num_batches):
+            batch = next(iterator)
+            losses = np.append(losses,model.get_losses(batch[0],batch[1]))
+        self.losses = np.array([np.random.rand() for i in range(len(self.train_img_names))])
+
+    def reduce_data(self,method,params=None):
         #reduce the data to the number of images wanted
         #this will be done by creating a mask for the data
         if method == "all":
@@ -95,46 +114,72 @@ class HAM10000Data():
             index = np.arange(0,len(self.train_img_names))
             np.random.shuffle(index)
             self.train_index_mask[index[:int(len(index)/2)]] = False
+        elif method == "loss":
+            #take the split of data between the params according to the loss
+            if params == None:
+                raise ValueError("Please specify the params as [lower percentage of data to use,upper percentage of data to use]")
+            self.train_index_mask = np.array([False]*len(self.train_img_names))
+            true_loss_indexes = np.argsort(self.losses)[int(params[0]*len(self.train_index_mask)):int(params[1]*len(self.train_index_mask))]
+            self.train_index_mask[true_loss_indexes] = True
+
         else:
             raise ValueError("Invalid method, please use 'all' or 'half'")
 
-
-    def init_data(self,bs,train=True,distributed=True):
+    def init_data(self,bs,train=True,distributed=True,shuffle=True):
         #take the data to be used in next epoch and create the data
         if train:
             file_path = self.train_data_dir
             img_names = self.train_img_names[self.train_index_mask]
             img_labels = self.train_img_labels[self.train_index_mask]
+
+            #find the number of data points
+            num_data = len(img_names)
+
+            if shuffle:
+                #shuffle the data (BE CAREFUL OF THIS WHEN COLLECTING DATA)
+                self.shuffle_index = np.arange(0,num_data)
+                np.random.shuffle(self.shuffle_index)
+                img_names = img_names[self.shuffle_index]
+                img_labels = img_labels[self.shuffle_index]
         else:
             file_path = self.test_data_dir
             img_names = self.test_img_names
             img_labels = self.test_img_labels
 
-        #find the number of data points
-        num_data = len(img_names)
-        
+            #find the number of data points
+            num_data = len(img_names)
 
+        
         #create the dataset
         def load_img(img_name):
             #load the image
-            img = tf.io.read_file(os.path.join(file_path,img_name+".jpg"))
+            img = tf.io.read_file(str(file_path)+"/"+img_name+".jpg")
+            img = tf.image.decode_jpeg(img, channels=self.img_size[-1])
+            img = tf.image.convert_image_dtype(img, tf.float32)
+            img = tf.image.resize(img, self.img_size[:-1])
             return img
         x_dataset = tf.data.Dataset.from_tensor_slices(img_names) #this will be the img names to use in training
         x_dataset = x_dataset.map(load_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)#this is now imgs
 
         y_dataset = tf.data.Dataset.from_tensor_slices(img_labels) #this will be the corrosponding labels
+        y_dataset = y_dataset.map(lambda x: tf.one_hot(x,depth=self.num_classes), num_parallel_calls=tf.data.experimental.AUTOTUNE)#this is now one hot encoded labels
 
         dataset = tf.data.Dataset.zip((x_dataset, y_dataset))
-        dataset = dataset.shuffle(buffer_size=num_data)
         dataset = dataset.batch(bs) #TODO is this to global batch size or per gpu batch size, needs testing
         if distributed:
             dataset = self.strategy.experimental_distribute_dataset(dataset)
         else:
             dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-        return dataset, num_data
+        return dataset, int(num_data/bs)
 
 
+
+class model():
+    def __init__(self):
+        pass
+    def get_losses(self,x,y):
+        return np.array([np.random.rand() for i in range(len(x))])
 
 def new_main():
 
@@ -142,13 +187,25 @@ def new_main():
     print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
     data_dir = "/com.docker.devenvironments.code/HAM10000/"
     preaugment_size = 1000
-    data = HAM10000Data(strategy,data_dir,preaugment_size)
-    test_dataset = data.init_data(10,train=False,distributed=False) #returns a dataset
-    train_dataset = data.init_data(12,train=True,distributed=True) #returns a dataset
-    iter_dataset = iter(train_dataset)
-    for i in range(5): #this needs to be the number of batches including the remainder
-        print(iter_dataset.get_next())
-    pass
+    data = Data(strategy,data_dir,preaugment_size)
+    m = model()
+    print(data.train_img_names)
+    print(data.train_img_labels)
+    test_dataset,test_batches = data.init_data(10,train=False,distributed=False) #returns a dataset
+    train_dataset,train_batches = data.init_data(12,train=True,distributed=True) #returns a dataset
+    print("Test dataset: ",test_dataset)
+    print("Train dataset: ",train_dataset)
+    print("Test num: ",test_batches)
+    print("Train num: ",train_batches)
+    data.get_loss(m)
+    print(len(data.losses))
+    train_dataset = data.reduce_data("loss",[0.1,0.2])
+    print(data.train_index_mask)
+    train_dataset,train_num = data.init_data(12,train=True,distributed=True) #returns a dataset
+    print("Train num: ",train_num)
+
+
+
 
 
 
